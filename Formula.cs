a12dd647121ae33300ea37656467c68abceea493
Robin Lindgren <rlindgren8@gmail.com>
2021-07-07 10:39:24 +0200

    Update Formula.cs

--- Formula.cs
+++ Formula.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace Formula
 {
     /// <summary>
-    /// Common methods that are used reguarly.
+    /// Common methods that are used regularly.
     /// </summary>
     public static class Common
     {
